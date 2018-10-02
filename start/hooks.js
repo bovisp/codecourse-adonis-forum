26f5c8dc83219d24cdcef703337dba305659ce23
Paul Bovis <paulbovisblahblah@gmail.com>
2018-10-01 19:56:12 -0600

    Create custom validation rule to check if tag exists when creating post

--- start/hooks.js
+++ start/hooks.js
@@ -8,4 +8,25 @@ hooks.after.providersBooted(() => {
 	Tag.all().then(tags => {
 		View.global('tags', tags)
 	}).catch(() => {})
+
+	const Validator = use('Validator')
+	const Database = use('Database')
+
+	const exists = async (data, field, message, args, get) => {
+		const value = get(data, field)
+
+		if (!value) {
+			return
+		}
+
+		const [table, column] = args
+
+		const found = await Database.table(table).where(column, value).first()
+
+		if (!found) {
+			throw message
+		}
+	}
+
+	Validator.extend('exists', exists)
 })
