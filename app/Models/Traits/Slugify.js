fe69dbbe4f890b69573515c4ea5fd9edc44c987c
Paul Bovis <paulbovisblahblah@gmail.com>
2018-10-01 19:33:35 -0600

    Add abaility to slugify post title via Trait

---
+++ app/Models/Traits/Slugify.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const { isEmpty } = require('lodash')
+const slugify = require('slugify')
+
+class Slugify {
+  register (Model, customOptions = {}) {
+    Model.addHook('afterCreate', (modelInstance) => {
+    	if (isEmpty(modelInstance.title)) {
+    		return
+    	} 
+
+    	let slugTitle = slugify(modelInstance.title, {
+    		lower: true
+    	})
+
+    	modelInstance.slug = `${slugTitle}-${modelInstance.id}`
+
+    	modelInstance.save()
+    })
+  }
+}
+
+module.exports = Slugify
